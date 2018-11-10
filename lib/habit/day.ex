00ed10f9401f502f502ef5a2bd777a99fb957022
Youthink <hufy3651@foxmail.com>
2018-11-10 16:38:25 +0800

    show unfinish and finish

--- lib/habit/day.ex
+++ lib/habit/day.ex
@@ -41,6 +41,23 @@ defmodule Habit.Day do
       join: f in subquery(finish_habit_id_query),
       where: h.id == f.habit_id,
       select: %{id: h.id, name: h.name, score: h.score, date: f.inserted_at, status: "finish"}
-    finish_habit_query |> Repo.all
+    finish_list  = finish_habit_query |> Repo.all
+
+    finish_habit_id_query_2 = from d in Day,
+      join: u in User,
+      where: u.open_id == ^open_id and fragment("?::date", d.inserted_at) >= ^date,
+      select: d.habit_id
+
+    arr = finish_habit_id_query_2 |> Repo.all
+
+    unfinish_habit_query = from h in Habit,
+      where: h.id not in ^arr,
+      distinct: h.id,
+      order_by: h.score,
+      select: %{id: h.id, name: h.name, score: h.score, status: "init"}
+    unfinish_list = unfinish_habit_query |> Repo.all
+
+    unfinish_list ++ finish_list
+
   end
 end
