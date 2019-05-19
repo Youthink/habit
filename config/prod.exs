0f2f4aceb1225a6d16a9c75bef13694bb8904aca
Youthink <hufy3651@foxmail.com>
2019-05-19 08:19:49 +0800

    on production debug info

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,8 @@ use Mix.Config
 config :habit, HabitWeb.Endpoint,
  #load_from_system_env: true,
   url: [scheme: "http", host: "api.iyearn.top", port: 14000],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  debug_errors: true
 
 # Do not print debug messages in production
 config :logger, level: :info
