edb4eab791451200cf792a8e397c8ddb354de54a
Youthink <hufy3651@foxmail.com>
2019-06-12 08:50:12 +0800

    allow github user no email

--- config/config.exs
+++ config/config.exs
@@ -27,7 +27,7 @@ config :phoenix, :json_library, Jason
 config :ueberauth, Ueberauth,
   base_path: "/api/v1/auth",
   providers: [
-    github: {Ueberauth.Strategy.Github, [send_redirect_uri: false]}
+    github: {Ueberauth.Strategy.Github, [send_redirect_uri: false, allow_private_emails: true]}
   ]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
